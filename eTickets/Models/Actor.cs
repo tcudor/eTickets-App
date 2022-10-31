74f920595b49c9ee776d6ae8501178da1e6b246d
Gheorghita Tudor <tudorgheorghita.vl@gmail.com>
2022-10-31 12:08:53 +0200

    add models, config database

---
+++ eTickets/Models/Actor.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ProfilePictureUrl { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Bio { get; set; }
+
+        //Relationships
+        public List<Actor_Movie> Actors_Movies { get; set; }
+    }
+}
---
+++ eTickets/Models/Producer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Producer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ProfilePictureUrl { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Bio { get; set; }
+
+        //Relationships
+        public List<Movie> Movies { get; set; }
+    }
+}
