4244df976f52711d721f43323ad2d5c7f7e97a22
tudorg <tudorg@indsoft.ro>
2022-12-27 15:42:36 +0200

    added controllers and views

--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -6,9 +6,13 @@ namespace eTickets.Models
     {
         [Key]
         public int Id { get; set; }
+        [Display(Name = "Profile Picture")]
         public string ProfilePictureUrl { get; set; }
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Display(Name = "Biography")]
         public string Bio { get; set; }
 
         //Relationships
