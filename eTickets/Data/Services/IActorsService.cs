45d0ea79ed2d45eec4a427a7480ec5b16bc5da49
tudorg <tudorg@indsoft.ro>
2022-12-30 16:11:19 +0200

    added BaseRepository, configuration of ActorsService

--- eTickets/Data/Services/IActorsService.cs
+++ eTickets/Data/Services/IActorsService.cs
@@ -1,14 +1,9 @@
 ﻿using eTickets.Models;
 using System.Threading.Tasks;
+using eTickets.Data.Base;
+using eTickets.Models;
 
 namespace eTickets.Data.Services
 {
-    public interface IActorsService
-    {
-        Task<IEnumerable<Actor>> GetAllAsync();
-        Task<Actor> GetByIdAsync(int id);
-        Task AddAsync(Actor actor);
-        Task<Actor> UpdateAsync(int id, Actor newActor);
-        Task DeleteAsync(int id);
-    }
+    public interface IActorsService : IEntityBaseRepository<Actor> { }
 }
--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -1,8 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
-
+using eTickets.Data.Base;
+using System;
 namespace eTickets.Models
 {
-    public class Actor
+    public class Actor : IEntityBase
     {
         [Key]
         public int Id { get; set; }
