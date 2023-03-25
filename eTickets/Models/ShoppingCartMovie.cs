7d7fff050d8087504ecee4ea68ce608eaa2bfd88
Gheorghita Tudor <tudorgheorghita.vl@gmail.com>
2023-03-25 18:48:40 +0200

    working login,logiut and signup for users

--- eTickets/Models/ShoppingCartMovie.cs
+++ eTickets/Models/ShoppingCartMovie.cs
@@ -8,8 +8,9 @@ namespace eTickets.Models
         public int Id { get; set; }
 
         public Movie? Movie { get; set; }
-        public int Amount { get; set; }
+        public int Amount { get; set; } 
         public string? ShoppingCartId { get; set; }
+
 
     }
 }
